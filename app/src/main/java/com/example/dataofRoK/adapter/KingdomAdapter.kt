556c55c4a4eaa05a8eaf929d99f22b3d41d0a285
NicoleRöhm <donjay1014@gmail.com>
2023-03-03 16:24:41 +0100

    Projekt App DataofRoK for Rise of Kingdoms

--- app/src/main/java/com/example/dataofRoK/adapter/KingdomAdapter.kt
+++ app/src/main/java/com/example/dataofRoK/adapter/KingdomAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.dataofRoK.adapter
 
-class KingdomAdapter {
+import android.view.ViewGroup
+
+class KingdomAdapter () {
+
+    class KingdomHolder
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int){
+
+    }
+
+    override fun onBindViewHolder(holder: , position: Int) {}
+
+
+
+    override fun getItemCount(): Int {
+        return
+    }
+
+
 }
